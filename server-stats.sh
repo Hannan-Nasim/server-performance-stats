d87a16378f69781ef06f12bb95950f030558fff5
Hannan-Nasim <128638238+Hannan-Nasim@users.noreply.github.com>
2025-05-29 02:42:43 +0500

    Get total number of logged in users currently

--- server-stats.sh
+++ server-stats.sh
@@ -2,3 +2,5 @@
 echo "Server Performance Stats:"
 echo "Total CPU usage:" 
 top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8"%"}'
+echo "Total number of current logged in users:"
+who -q | grep "# users" | grep -o '[0-9]\+'
