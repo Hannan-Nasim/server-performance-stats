106e9aebd15c11ad2606e3b3161dc6f238f446a7
Hannan-Nasim <128638238+Hannan-Nasim@users.noreply.github.com>
2025-05-31 17:16:44 +0500

    Add server status stats and improve Total CPU usage command

--- server-stats.sh
+++ server-stats.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 echo "Server Performance Stats:"
+echo "-------------------------"
+echo ""
 echo "Total CPU usage:" 
-top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8"%"}'
+echo "----------------"
+top -bn1 | awk -F',' '/Cpu\(s\)/ {for(i=1; i<=NF; i++) {if ($i ~ /id/) {split($i, a, " "); idle = a[1]; usage = 100 - idle; printf("%.1f\n", usage)}}}'
+echo ""
+echo "OS Version:"
+echo "-----------"
+awk -F= '/^VERSION=/ { gsub(/"/, "", $2); print $2 }' /etc/os-release
+echo ""
 echo "Total number of current logged in users:"
+echo "----------------------------------------"
 who -q | grep "# users" | grep -o '[0-9]\+'
