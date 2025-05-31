dff3d7e21b0143c8bea3ac570788b6be6fc9054b
Hannan-Nasim <128638238+Hannan-Nasim@users.noreply.github.com>
2025-05-31 17:30:18 +0500

    Add stats for memory usage

--- server-stats.sh
+++ server-stats.sh
@@ -6,6 +6,10 @@ echo "Total CPU usage:"
 echo "----------------"
 top -bn1 | awk -F',' '/Cpu\(s\)/ {for(i=1; i<=NF; i++) {if ($i ~ /id/) {split($i, a, " "); idle = a[1]; usage = 100 - idle; printf("%.1f\n", usage)}}}'
 echo ""
+echo "Total memory usage:" 
+echo "----------------"
+free -h | awk 'NR==2 { total=$2; used=$3; avail=$7; perc=used/total*100; printf("Total: %s, Used: %s, Available: %s, Usage: %.2f%%\n", total, used, avail, perc) }'
+echo ""
 echo "Total disk usage:" 
 echo "----------------"
 df -h / | tail -1 | awk '{print "Total: "$2", Used: "$3", Free: "$4", Usage: "$5}'
