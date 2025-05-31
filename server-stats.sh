83005dd61518e7b75101e6ad314d2e619fd4f170
Hannan-Nasim <128638238+Hannan-Nasim@users.noreply.github.com>
2025-05-31 17:21:45 +0500

    Add Total disk usage

--- server-stats.sh
+++ server-stats.sh
@@ -6,6 +6,10 @@ echo "Total CPU usage:"
 echo "----------------"
 top -bn1 | awk -F',' '/Cpu\(s\)/ {for(i=1; i<=NF; i++) {if ($i ~ /id/) {split($i, a, " "); idle = a[1]; usage = 100 - idle; printf("%.1f\n", usage)}}}'
 echo ""
+echo "Total disk usage:" 
+echo "----------------"
+df -h / | tail -1 | awk '{print "Total: "$2", Used: "$3", Free: "$4", Usage: "$5}'
+echo ""
 echo "OS Version:"
 echo "-----------"
 awk -F= '/^VERSION=/ { gsub(/"/, "", $2); print $2 }' /etc/os-release
